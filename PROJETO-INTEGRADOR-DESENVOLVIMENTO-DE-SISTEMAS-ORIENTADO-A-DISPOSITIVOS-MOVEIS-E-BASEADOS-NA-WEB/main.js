4ae112568c2c3784a51c8c0c59d0cb87d4bcaa43
Rafael Arduini <rafaelarduini@hotmail.com>
2022-05-18 19:07:35 -0300

    Refatoração método criaLinha

--- PROJETO-INTEGRADOR-DESENVOLVIMENTO-DE-SISTEMAS-ORIENTADO-A-DISPOSITIVOS-MOVEIS-E-BASEADOS-NA-WEB/main.js
+++ PROJETO-INTEGRADOR-DESENVOLVIMENTO-DE-SISTEMAS-ORIENTADO-A-DISPOSITIVOS-MOVEIS-E-BASEADOS-NA-WEB/main.js
@@ -7,41 +7,21 @@ function get(url) {
 
 function criaLinha(pergunta) {
 
-    divLinha = document.createElement("div");
-    divLinha.setAttribute('class', 'ic-class-form-row col-12')
+  let linha = document.createElement("div");
+  linha.setAttribute('class', 'ic-class-form-row col-12')
 
-    divFormCheck = document.createElement("div");
-    divFormCheck.setAttribute('class', 'ic-class-form-check-legal form-check form-check-inline')
+  linha.innerHTML = `    
+    <div class="ic-class-form-check-legal form-check form-check-inline">
+        <div class="col-10 m-0 p-0">
+            <label class="ic-class-form-check-label-legal form-check-label" for="${pergunta.id}">${pergunta.pergunta}</label>
+        </div>
+        <div class="col-2 text-center m-0 p-0 justify-content-center align-items-center">
+            <input id="${pergunta.id}" type="checkbox" class="ic-class-form-check-input-legal"
+            data-ic-form-field="${pergunta.id}" name="${pergunta.id}" />
+        </div>                             
+    </div>`;
 
-    divLabel = document.createElement("div");
-    divLabel.setAttribute('class', 'col-10 m-0 p-0')
-
-    var label = document.createElement('label');
-    label.setAttribute('class', 'ic-class-form-check-label-legal form-check-label')
-    label.setAttribute('for', pergunta.id)
-
-    label.innerHTML = pergunta.pergunta
-    
-    divLabel.appendChild(label);
-
-    divCheckBox = document.createElement("div");
-    divCheckBox.setAttribute('class', 'col-2 text-center m-0 p-0 justify-content-center align-items-center')
-   
-    var checkBox = document.createElement('input');
-    checkBox.setAttribute('id', pergunta.id)
-    checkBox.setAttribute('type','checkbox')
-    checkBox.setAttribute('class','ic-class-form-check-input-legal')
-    checkBox.setAttribute('data-ic-form-field', pergunta.id)
-    checkBox.setAttribute('name', pergunta.id)
-
-    divCheckBox.appendChild(checkBox);
-
-    divFormCheck.appendChild(divLabel);
-    divFormCheck.appendChild(divCheckBox);
-
-    divLinha.appendChild(divFormCheck);
-
-    return divLinha;
+    return linha;
 }
 
 function main() {
