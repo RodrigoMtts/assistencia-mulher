c2413874579d2aa5d2919dddb874357bf63f637d
rodrigo <rodrigo@MeuDebian>
2022-05-22 13:37:32 -0300

    V0.9

--- PROJETO-INTEGRADOR-DESENVOLVIMENTO-DE-SISTEMAS-ORIENTADO-A-DISPOSITIVOS-MOVEIS-E-BASEADOS-NA-WEB/main.js
+++ PROJETO-INTEGRADOR-DESENVOLVIMENTO-DE-SISTEMAS-ORIENTADO-A-DISPOSITIVOS-MOVEIS-E-BASEADOS-NA-WEB/main.js
@@ -7,7 +7,6 @@ function get(url) {
 
 function criaLinha(pergunta) {
 
-<<<<<<< HEAD
   let linha = document.createElement("div");
   linha.setAttribute('class', 'ic-class-form-row col-12')
 
@@ -23,43 +22,6 @@ function criaLinha(pergunta) {
     </div>`;
 
     return linha;
-=======
-  divLinha = document.createElement("div");
-  divLinha.setAttribute('class', 'ic-class-form-row col-12')
-
-  divFormCheck = document.createElement("div");
-  divFormCheck.setAttribute('class', 'ic-class-form-check-legal form-check form-check-inline')
-
-  divLabel = document.createElement("div");
-  divLabel.setAttribute('class', 'col-10 m-0 p-0')
-
-  var label = document.createElement('label');
-  label.setAttribute('class', 'ic-class-form-check-label-legal form-check-label')
-  label.setAttribute('for', pergunta.id)
-
-  label.innerHTML = pergunta.pergunta
-
-  divLabel.appendChild(label);
-
-  divCheckBox = document.createElement("div");
-  divCheckBox.setAttribute('class', 'col-2 text-center m-0 p-0 justify-content-center align-items-center')
-
-  var checkBox = document.createElement('input');
-  checkBox.setAttribute('id', pergunta.id)
-  checkBox.setAttribute('type', 'checkbox')
-  checkBox.setAttribute('class', 'ic-class-form-check-input-legal')
-  checkBox.setAttribute('data-ic-form-field', pergunta.id)
-  checkBox.setAttribute('name', pergunta.id)
-
-  divCheckBox.appendChild(checkBox);
-
-  divFormCheck.appendChild(divLabel);
-  divFormCheck.appendChild(divCheckBox);
-
-  divLinha.appendChild(divFormCheck);
-
-  return divLinha;
->>>>>>> master
 }
 
 function main() {
@@ -68,20 +30,10 @@ function main() {
   let perguntas = JSON.parse(data);
 
   let listaPerguntas = document.getElementById("listaPerguntas");
-<<<<<<< HEAD
   perguntas.forEach(element => {
     let linha = criaLinha(element);
     listaPerguntas.appendChild(linha);
   });
-=======
-
-  if(listaPerguntas){
-    perguntas.forEach(element => {
-      let linha = criaLinha(element);
-      listaPerguntas.appendChild(linha);
-    });
-  }    
->>>>>>> online/master
 }
 
 const form = document.querySelector("#form");
@@ -97,7 +49,6 @@ form.addEventListener("submit", (event) => {
   console.log(json[12])
 
   const options = {
-<<<<<<< HEAD
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -124,38 +75,6 @@ form.addEventListener("submit", (event) => {
 
   console.log("A abaixo")
   console.log(a)
-=======
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(json)
-    }
-    
-  fetch('http://localhost:8080/resposta', options)
-  .then( res =>  {
-    if(res.status === 201){
-      alert("Dados salvos com sucesso")
-      return res.json();
-    }else{
-      alert("Algo deu errado, tente novamente mais tarde")
-      return new Error("Deu ruim")
-    }
-  })
-  .catch(function(err){
-    console.log("Erro no fetch")
-    return new Error("Deu ruim " + err)
-  }).then(corpo => {   
-
-    let resposta = 'nenhuma'
-
-    if (corpo !== ''){
-      resposta = corpo
-    }
-
-    window.location.href=`resposta.html?violencias=${resposta}`;
-  })
->>>>>>> online/master
 })
 
 main()
