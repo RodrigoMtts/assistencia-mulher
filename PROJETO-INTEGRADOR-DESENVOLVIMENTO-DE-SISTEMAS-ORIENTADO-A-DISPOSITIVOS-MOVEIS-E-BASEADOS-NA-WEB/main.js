6c8a40a9491b6403e28d38cb33e1bc772b56b1f5
rodrigo <rodrigo@MeuDebian>
2022-05-22 13:38:11 -0300

    V0.9

--- PROJETO-INTEGRADOR-DESENVOLVIMENTO-DE-SISTEMAS-ORIENTADO-A-DISPOSITIVOS-MOVEIS-E-BASEADOS-NA-WEB/main.js
+++ PROJETO-INTEGRADOR-DESENVOLVIMENTO-DE-SISTEMAS-ORIENTADO-A-DISPOSITIVOS-MOVEIS-E-BASEADOS-NA-WEB/main.js
@@ -7,7 +7,6 @@ function get(url) {
 
 function criaLinha(pergunta) {
 
-<<<<<<< HEAD
   let linha = document.createElement("div");
   linha.setAttribute('class', 'ic-class-form-row col-12')
 
@@ -23,43 +22,6 @@ function criaLinha(pergunta) {
     </div>`;
 
     return linha;
-=======
-  divLinha = document.createElement("div");
-  divLinha.setAttribute('class', 'ic-class-form-row col-12')
-
-  divFormCheck = document.createElement("div");
-  divFormCheck.setAttribute('class', 'ic-class-form-check-legal form-check form-check-inline')
-
-  divLabel = document.createElement("div");
-  divLabel.setAttribute('class', 'col-10 m-0 p-0')
-
-  var label = document.createElement('label');
-  label.setAttribute('class', 'ic-class-form-check-label-legal form-check-label')
-  label.setAttribute('for', pergunta.id)
-
-  label.innerHTML = pergunta.pergunta
-
-  divLabel.appendChild(label);
-
-  divCheckBox = document.createElement("div");
-  divCheckBox.setAttribute('class', 'col-2 text-center m-0 p-0 justify-content-center align-items-center')
-
-  var checkBox = document.createElement('input');
-  checkBox.setAttribute('id', pergunta.id)
-  checkBox.setAttribute('type', 'checkbox')
-  checkBox.setAttribute('class', 'ic-class-form-check-input-legal')
-  checkBox.setAttribute('data-ic-form-field', pergunta.id)
-  checkBox.setAttribute('name', pergunta.id)
-
-  divCheckBox.appendChild(checkBox);
-
-  divFormCheck.appendChild(divLabel);
-  divFormCheck.appendChild(divCheckBox);
-
-  divLinha.appendChild(divFormCheck);
-
-  return divLinha;
->>>>>>> master
 }
 
 function main() {
