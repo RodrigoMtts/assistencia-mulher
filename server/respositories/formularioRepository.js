117b4a4170fdd97214ecce0ae623df81e69a02b9
rodrigo <rodrigo@MeuDebian>
2022-05-22 13:05:29 -0300

    add module config

--- server/respositories/formularioRepository.js
+++ server/respositories/formularioRepository.js
@@ -1,12 +1,12 @@
 import db from '../databases/mysql.js'
 
 const formularioRepository = {
-    //Método que pega as peruntas do formualário
-    listaPerguntas() {
-        return db.findAll({ attributes: ['id', "pergunta"] }).then()
-    }
-    /*Deve-se criar o método que adcionar os valores do formulário
-    no banco de dados*/
+  //Método que pega as peruntas do formualário
+  listaPerguntas() {
+    return db.findAll({ attributes: ['id', "pergunta"] }).then()
+  }
+  /*Deve-se criar o método que adcionar os valores do formulário
+  no banco de dados*/
 }
 
 export default formularioRepository
